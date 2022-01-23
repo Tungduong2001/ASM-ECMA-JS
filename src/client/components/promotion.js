761f9b1dd087ac062c6112ca63855878468991fa
Bui Kha Tung Duong <duongbktph13295@fpt.edu.vn>
2022-01-23 17:06:34 +0700

    delete  bg-color

--- src/client/components/promotion.js
+++ src/client/components/promotion.js
@@ -1,7 +1,7 @@
 const Promotion = {
     render() {
         return /* html */ `
-            <div class="py-0 px-48 bg-[url(https://images.fpt.shop/unsafe/fit-in/filters:quality(80):fill(transparent)/fptshop.com.vn/Uploads/images/2015/CTKM-Voucher/Sale%20het%20an%20tet/Sieu%20Sale%20Mung%20Nam%20Moi/bg-desk.png)]">
+            <div class="py-0 px-48 ">
                 <a href="">
                     <img src="https://images.fpt.shop/unsafe/fit-in/filters:quality(80):fill(transparent)/fptshop.com.vn/Uploads/images/2015/CTKM-Voucher/Sale%20het%20an%20tet/Sieu%20Sale%20Mung%20Nam%20Moi/banner-desk.png"
                     alt="">
