3f8f7071906ba7a6e53f65d27cc0919a7824b008
Bui Kha Tung Duong <duongbktph13295@fpt.edu.vn>
2022-01-25 16:53:59 +0700

    fix css accessory

--- src/client/components/accessory.js
+++ src/client/components/accessory.js
@@ -8,49 +8,49 @@ const HotAccessory = {
                 <div class="box-accessory">
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-crown text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-crown text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Phụ kiện nổi bật</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-clone text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-clone text-[36px] text-slate-400 mt-7"></i>
                             <p class="font-medium mt-6">Bao da ốp lưng</p>
                         </div>
                     </a>
                     <a href="" > 
                         <div class="accessory">
-                            <i class="fas fa-battery-three-quarters text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-battery-three-quarters text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Pin dự phòng</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-sd-card text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-sd-card text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Thẻ nhớ</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fab fa-apple text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fab fa-apple text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Phụ kiện Apple</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-mobile text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-mobile text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Miếng dán màn hình</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-headphones text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-headphones text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Tai nghe</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-print text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-print text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Mực in</p>
                         </div>
                     </a>
@@ -59,49 +59,49 @@ const HotAccessory = {
                 <div class="box-accessory">
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-volume-up text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-volume-up text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Loa</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-hdd text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-hdd text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">USB - Ổ cứng</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fab fa-usb text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fab fa-usb text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Sạc cáp</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-mouse text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-mouse text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Chuột</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-keyboard text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-keyboard text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Bàn phím</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-suitcase text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-suitcase text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Balo - Túi xách</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-tv text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-tv text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">TV BOX</p>
                         </div>
                     </a>
                     <a href="" >
                         <div class="accessory">
-                            <i class="fas fa-ellipsis-h text-[36px] text-slate-400 leading-[150px] mt-7"></i>
+                            <i class="fas fa-ellipsis-h text-[36px] text-slate-400  mt-7"></i>
                             <p class="font-medium mt-6">Phụ kiện khác</p>
                         </div>
                     </a>
